e9c2b2a80d5f2b994b5da14c49b479549eb1630f
Eomjaeyoon <goodeom01@naver.com>
2024-08-30 03:44:07 +0900

    "masonry"

--- src/Pages/landing/LandingPresentation.tsx
+++ src/Pages/landing/LandingPresentation.tsx
@@ -2,31 +2,62 @@ import React from "react";
 import {
   Box,
   Button,
+  Grid,
+  SimpleGrid,
   Container,
-  Flex,
   Heading,
+  Text,
   HStack,
-  Icon,
-  Link,
+  Flex,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Wrap,
+  WrapItem,
   useColorModeValue,
 } from "@chakra-ui/react";
+import Masonry from "react-masonry-css";
 import { useNavigate } from "react-router-dom";
 import { NavigateFunction } from "react-router-dom";
+import "./landing.css";
 
 interface LandingPresentationProps {
   navigate: NavigateFunction;
+  post: Post[];
+  handlePostClick: (post: Post) => void;
+  isPostModalOpen: boolean;
+  onPostModalClose: () => void;
+  selectedPost: Post | null;
+}
+interface Post {
+  id: number;
+  title: string;
+  content: string;
+  categories: string[];
+  views: number;
 }
 
 const LandingPresentation: React.FC<LandingPresentationProps> = (props) => {
   const navigate = useNavigate();
 
-  const bgColor = useColorModeValue("blue.500", "blue.700");
+  const bgColor = useColorModeValue("blue.500", "blue.3000");
   const textColor = useColorModeValue("white", "gray.100");
   const modalBg = useColorModeValue("white", "gray.800");
-  const inputBg = useColorModeValue("gray.100", "gray.700");
+  const inputBg = useColorModeValue("gray.100", "gray.3000");
+
+  const breakpointColumnsObj = {
+    default: 3,
+    1300: 2,
+    800: 1,
+  };
 
   return (
     <Box>
+      {/* header */}
       <Box
         as="header"
         position="fixed"
@@ -83,7 +114,75 @@ const LandingPresentation: React.FC<LandingPresentationProps> = (props) => {
           </Flex>
         </Container>
       </Box>
-      <Button onClick={() => navigate("/project")}>프로젝트페이지</Button>
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {props.post.map((post) => (
+          <Box
+            key={post.id}
+            padding="20px"
+            border="2px"
+            borderRadius="5px"
+            borderColor="gray.200"
+            marginBottom="10px"
+            bg="white"
+            overflow="hidden"
+            onClick={() => props.handlePostClick(post)}
+          >
+            <Heading margin="0 0 10px 0">{post.title}</Heading>
+            {post.categories.map((category) => (
+              <Button
+                key={category}
+                size="xs"
+                colorScheme="gray"
+                textAlign="center"
+                margin="0 5px 10px 0"
+              >
+                {category}
+              </Button>
+            ))}
+            <Text>{post.content}</Text>
+          </Box>
+        ))}
+      </Masonry>
+
+      {/* Post Modal */}
+      <Modal
+        isOpen={props.isPostModalOpen}
+        onClose={props.onPostModalClose}
+        size="xl"
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{props.selectedPost?.title}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>{props.selectedPost?.content}</Text>
+            <Text mt={4} fontWeight="bold">
+              카테고리:
+            </Text>
+            <Wrap mt={2}>
+              {props.selectedPost?.categories.map((category) => (
+                <WrapItem key={category}>
+                  <Button size="xs" colorScheme="blue" variant="outline">
+                    {category}
+                  </Button>
+                </WrapItem>
+              ))}
+            </Wrap>
+            <Text mt={4} fontWeight="bold">
+              조회수: {(props.selectedPost?.views ?? 0) + 1}
+            </Text>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={props.onPostModalClose}>
+              닫기
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 };
