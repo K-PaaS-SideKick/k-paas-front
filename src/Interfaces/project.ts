070e596c9fe6217055f5d4a100986fe271f72542
Keonyoung <joseph12752@gmail.com>
2024-10-29 23:39:45 +0900

    Project api: search, create, getMain, delete, update

--- src/Interfaces/project.ts
+++ src/Interfaces/project.ts
@@ -1,8 +1,3 @@
-export interface JoinRequest {
-    pid: number;
-    uid: string;
-};
-
 export interface StatusResponse {
     uid: string;
     active: boolean;
@@ -12,3 +7,77 @@ export interface JoinResponse {
     pid: number;
     uid: string;
 };
+
+export interface SearchResponse {
+    totalPages: number;
+    totalElements: number;
+    sort: {
+        sorted: boolean;
+        empty: boolean;
+        unsorted: boolean;
+    };
+    first: boolean;
+    last: boolean;
+    pageable: {
+        sort: {
+            sorted: boolean;
+            empty: boolean;
+            unsorted: boolean;
+        };
+        paged: boolean;
+        pageNumber: number;
+        pageSize: number;
+        offset: number;
+        unpaged: boolean;
+    };
+    size: number;
+    content: NewProjectResponse[];
+    number: number;
+    numberOfElements: number;
+    empty: boolean;
+};
+
+export interface NewProjectResponse {
+    pid: number;
+    uid: string;
+    title: string;
+    date: string;
+    content: string;
+    repo_link: string;
+    upvotes: number;
+    comments: number;
+    views: number;
+    scraps: number;
+    status: string;
+    max_members: number;
+    current_members: number;
+};
+
+export interface MainPageResponse {
+    totalPages: number;
+    totalElements: number;
+    sort: {
+      sorted: boolean;
+      empty: boolean;
+      unsorted: boolean;
+    };
+    first: boolean;
+    last: boolean;
+    pageable: {
+      sort: {
+        sorted: boolean;
+        empty: boolean;
+        unsorted: boolean;
+      };
+      paged: boolean;
+      pageNumber: number;
+      pageSize: number;
+      offset: number;
+      unpaged: boolean;
+    };
+    size: number;
+    content: NewProjectResponse[];
+    number: number;
+    numberOfElements: number;
+    empty: boolean;
+  }
